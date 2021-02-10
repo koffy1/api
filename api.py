8c70bcfe32806ab0dc8d5033d5e8f80ee09fd98a
Sholesi Kofoworola <kofoworola.sholesi@cottacush.com>
2021-02-10 20:01:56 +0100

    learning flask for API purpose

--- api.py
+++ api.py
@@ -4,16 +4,31 @@ from flask import request, jsonify, json
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+bookPath = '/var/www/projects/api/resources/books'
+with open(bookPath, encoding='utf-8') as bookFile:
+    allBooks = json.loads(bookFile.read())
+
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
 
 @app.route('/resources/books', methods=['GET'])
-def books():
-    bookPath = '/var/www/projects/api/resources/books'
-    with open(bookPath, encoding='utf-8') as bookFile:
-        books = json.loads(bookFile.read())
+def getBooks():
+    
+    return jsonify(allBooks)
 
-    return jsonify(books)
+@app.route('/resources/book', methods=['GET'])
+def getBook():
+    if 'id' in request.args:
+        id = int(request.args['id'])
+    else:
+        return "Error: No id field provided. Please specify an id."
+    
+    result = []
+    for book in allBooks:
+        if book['id'] == id:
+            result.append(book)
+    
+    return jsonify(result)
 
 app.run()
